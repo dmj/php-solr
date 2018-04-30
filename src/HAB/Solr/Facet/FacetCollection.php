69eb56d81faa4aa4e67bd9fc738af4dd8d22f394
David Maus <maus@hab.de>
2018-04-30 10:14:30 +0200

    Add missing $filter class property to FacetCollection

--- src/HAB/Solr/Facet/FacetCollection.php
+++ src/HAB/Solr/Facet/FacetCollection.php
@@ -51,6 +51,13 @@ class FacetCollection implements Countable, IteratorAggregate, ParameterProvider
     private $facets;
 
     /**
+     * Component state filter.
+     *
+     * @var StateFilter\StateFilterInterface
+     */
+    private $filter;
+
+    /**
      * Constructor.
      *
      * @return void
